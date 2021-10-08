591217ac24af0fc25e828f76f13f163c9e65bf40
TheMax404 <massimiliano.borgese@gmail.com>
2021-10-08 17:53:20 +0200

    finalmente funziona

--- src/main/java/it/beije/herse/web/HelloServlet.java
+++ src/main/java/it/beije/herse/web/HelloServlet.java
@@ -7,16 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
+
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public HelloServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -26,8 +22,9 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String s = "<h1>Finalmente Funziona</h1>";
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append(s);
 	}
 
 	/**
